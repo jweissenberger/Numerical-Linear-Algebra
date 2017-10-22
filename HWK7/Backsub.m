33ff3edd37bb28db4ba3da8c1ba9b23838d6ee62
John Weissenberger <johnaweissenberger@gmail.com>
2017-10-22 19:46:36 -0400

    start HW

---
+++ HWK7/Backsub.m
@@ -0,0 +1,29 @@
+function [x] = Backsub(R,y)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Solving by back substitution
+%
+%   Inputs
+%   1. R= nxn upper triangular matrix
+%   2. y= nx1 vector
+%
+%   Output
+%   1. x= nx1 vector of solution to Rx=y
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[n,~]=size(y);
+
+
+for j=n:-1:1,
+    x(j)=y(j);
+    
+    for k=j+1:n,
+        x(j)=x(j)-R(j,k)*x(k);
+    end
+    x(j)=x(j)/R(j,j);
+end
+
+end
+
