4034184ce453c0e5838901425050fcf09806431e
John Weissenberger <johnaweissenberger@gmail.com>
2017-10-31 16:04:14 -0400

    Fixed hwk8

--- HWK8/P133.m
+++ HWK8/P133.m
@@ -3,9 +3,9 @@
 % plot each funtion individually to see how the computer generates
 % different values for two functions that should be identical
 
-x = -1.920:.001:2.080;
+x = linspace(1.920,2.080,160);
 
-p = x.^9 - 18.*x.^8 + 144.*x.^7 - 672.*x.^6 + 2016.*x.^5 - 4032.*x.^4 + 5376.*x.^3 - 4608.*x.^2 + 2304.*x - 512;
+p = (x.^9) - (18.*x.^8) + (144.*x.^7) - (672.*x.^6) + (2016.*x.^5) - (4032.*x.^4) + (5376.*x.^3) - (4608.*x.^2) + (2304.*x) - 512;
 
 plot(x, p)
 
@@ -13,6 +13,6 @@ hold on
 
 P = (x-2).^9;
 
-plot(P, x)
+plot(x,P)
 
 hold off
