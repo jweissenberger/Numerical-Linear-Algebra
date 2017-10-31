d0d9a28f6e42cda80e72cdda08a4128df693494f
John Weissenberger <johnaweissenberger@gmail.com>
2017-10-31 15:58:04 -0400

    HWK 7

--- HWK7/randMatrix123.m
+++ HWK7/randMatrix123.m
@@ -1,13 +1,15 @@
 
-%A = randn(m,m)/sqrt(m);
+% 12.3 Random Matrix
 
-m =1:10;
+m =1:100;
 m(1) = 8;
 val = 8;
 
-for i = 2 : 10
+for i = 2 : 100
     val  = val * 2;
     m(i) = val;
     A = randn(m(i),m(i))/sqrt(m(i));
+    scatter(eig(A))
+    hold on
 end
-
+hold off
